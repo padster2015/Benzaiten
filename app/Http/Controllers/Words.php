cb2b37a6c4fb4be45170d26f63268adc5a063db3
Patrick Henry <git@patrickhenry.co.uk>
2015-12-20 22:36:40 +0000

    hello

--- app/Http/Controllers/Words.php
+++ app/Http/Controllers/Words.php
@@ -2,80 +2,86 @@
 
 namespace App\Http\Controllers;
 
-use App\Employee;
 use Illuminate\Http\Request;
+
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class Employees extends Controller {
-
+class Words extends Controller
+{
     /**
      * Display a listing of the resource.
      *
-     * @return Response
+     * @return \Illuminate\Http\Response
      */
-    public function index($ID = null) {
-    echo "hello";
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  Request  $request
-     * @return Response
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
-    public function store(Request $request) {
-        $employee = new Employee;
-
-        $employee->name = $request->input('name');
-        $employee->email = $request->input('email');
-        $employee->contact_number = $request->input('contact_number');
-        $employee->position = $request->input('position');
-        $employee->save();
-
-        return 'Employee record successfully created with id ' . $employee->id;
+    public function store(Request $request)
+    {
+        //
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return Response
+     * @return \Illuminate\Http\Response
      */
-    public function show($id) {
-        return Employee::find($id);
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  Request  $request
+     * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return Response
+     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id) {
-        $employee = Employee::find($id);
-
-        $employee->name = $request->input('name');
-        $employee->email = $request->input('email');
-        $employee->contact_number = $request->input('contact_number');
-        $employee->position = $request->input('position');
-        $employee->save();
-
-        return "Sucess updating user #" . $employee->id;
+    public function update(Request $request, $id)
+    {
+        //
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return Response
+     * @return \Illuminate\Http\Response
      */
-    public function destroy(Request $request) {
-        $employee = Employee::find($request->input('id'));
-
-        $employee->delete();
-
-        return "Employee record successfully deleted #" . $request->input('id');
+    public function destroy($id)
+    {
+        //
     }
-} 
+}
