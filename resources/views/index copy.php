0865daa1dc810fcab72e30cc1e824892b1cb186c
paddy <paddy@MAC21.local>
2015-12-21 00:20:45 -0500

    lighter and smaller

---
+++ resources/views/index copy.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en-US" ng-app="employeeRecords">
+    <head>
+        <title>Rakuten Marketing : Translation Platform</title>
+
+      <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <h2>Benzatain - Rakuten Marketing Translation</h2>
+        <div  ng-controller="employeesController">
+
+            <!-- Table-to-load-the-data Part -->
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>English Word</th>
+                        <th>French</th>
+                        <th>German</th>
+                        <th>Japanese</th>
+                        <th><button id="btn-add" class="btn btn-primary btn-xs" ng-click="toggle('add', 0)">Add New Word</button></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr ng-repeat="word in words">
+                        <td>{{ word.ID }}</td>
+                        <td>{{ word.Source }}</td>
+                        <td>{{ word.Target }}</td>
+                        <td>{{ word.Last_edit_by }}</td>
+                        <td>{{ word.Platform }}</td>
+                        <td>
+                            <button class="btn btn-default btn-xs btn-detail" ng-click="toggle('edit', word.id)">Edit</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <!-- End of Table-to-load-the-data Part -->
+            <!-- Modal (Pop up when detail button clicked) -->
+            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                            <h4 class="modal-title" id="myModalLabel">{{form_title}}</h4>
+                        </div>
+                        <div class="modal-body">
+                            <form name="frmEmployees" class="form-horizontal" novalidate="">
+
+                                <div class="form-group error">
+                                    <label for="inputEmail3" class="col-sm-3 control-label">Name</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control has-error" id="name" name="name" placeholder="Fullname" value="{{name}}" 
+                                        ng-model="employee.name" ng-required="true">
+                                        <span class="help-inline" 
+                                        ng-show="frmEmployees.name.$invalid && frmEmployees.name.$touched">Name field is required</span>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="inputEmail3" class="col-sm-3 control-label">Email</label>
+                                    <div class="col-sm-9">
+                                        <input type="email" class="form-control" id="email" name="email" placeholder="Email Address" value="{{email}}" 
+                                        ng-model="employee.email" ng-required="true">
+                                        <span class="help-inline" 
+                                        ng-show="frmEmployees.email.$invalid && frmEmployees.email.$touched">Valid Email field is required</span>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="inputEmail3" class="col-sm-3 control-label">Contact Number</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" id="contact_number" name="contact_number" placeholder="Contact Number" value="{{contact_number}}" 
+                                        ng-model="employee.contact_number" ng-required="true">
+                                    <span class="help-inline" 
+                                        ng-show="frmEmployees.contact_number.$invalid && frmEmployees.contact_number.$touched">Contact number field is required</span>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="inputEmail3" class="col-sm-3 control-label">Position</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" id="position" name="position" placeholder="Position" value="{{position}}" 
+                                        ng-model="employee.position" ng-required="true">
+                                    <span class="help-inline" 
+                                        ng-show="frmEmployees.position.$invalid && frmEmployees.position.$touched">Position field is required</span>
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary" id="btn-save" ng-click="save(modalstate, id)" ng-disabled="frmEmployees.$invalid">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Load Javascript Libraries (AngularJS, JQuery, Bootstrap) -->
+<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+        
+        <!-- AngularJS Application Scripts -->
+        <script src="<?= asset('app/app.js') ?>"></script>
+        <script src="<?= asset('app/controllers/words.js') ?>"></script>
+    </body>
+</html>
