6d42bbf054ca5503664ca1370ef3570ae292e3ab
paddy <paddy@MAC21.local>
2015-12-21 00:47:11 -0500

    query

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+</head>
 <body>
 
 <div ng-app="myApp" ng-controller="customersCtrl"> 
