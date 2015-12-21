cc2132ac7a405bcbd5a470fc7b9f255b49d79594
paddy <paddy@MAC21.local>
2015-12-21 01:05:04 -0500

    more advance

--- resources/views/index.php
+++ resources/views/index.php
@@ -46,7 +46,8 @@
     
     
     <h1 style="float:left;margin-top:11px;">Rakuten Marketing Translate Platform</h1>
-    
+            <h2>Benzatain</h2>
+
     
     <div id="controllers">
       <a href="resetpassword.php">
@@ -59,12 +60,115 @@
       
 
 <div class="app-content" id="data-items" ng-app="" ng-controller="customersController"> 
-       <div class="grid" ng-repeat="x in names">   
+       <div class="grid" >   
          <div class="col-sm-10">
           <!-- Language Flag -->  
           <p class="pname">{{x.ID}}</p>
             </div>
             
+
+<table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>English Word</th>
+                        <th>French</th>
+                        <th>German</th>
+                        <th>Japanese</th>
+                        <th><button id="btn-add" class="btn btn-primary btn-xs" ng-click="toggle('add', 0)">Add New Word</button></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr ng-repeat="x in names">
+                        <td>{{ word.ID }}</td>
+                        <td>{{ word.Source }}</td>
+                        <td>{{ word.Target }}</td>
+                        <td>{{ word.Last_edit_by }}</td>
+                        <td>{{ word.Platform }}</td>
+                        <td>
+                            <button class="btn btn-default btn-xs btn-detail" ng-click="toggle('edit', word.id)">Edit</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <!-- End of Table-to-load-the-data Part -->
+            <!-- Modal (Pop up when detail button clicked) -->
+            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                            <h4 class="modal-title" id="myModalLabel">{{form_title}}</h4>
+                        </div>
+                        <div class="modal-body">
+                            <form name="frmEmployees" class="form-horizontal" novalidate="">
+
+                                <div class="form-group error">
+                                    <label for="inputEmail3" class="col-sm-3 control-label">Name</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control has-error" id="name" name="name" placeholder="Fullname" value="{{name}}" 
+                                        ng-model="employee.name" ng-required="true">
+                                        <span class="help-inline" 
+                                        ng-show="frmEmployees.name.$invalid && frmEmployees.name.$touched">Name field is required</span>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="inputEmail3" class="col-sm-3 control-label">Email</label>
+                                    <div class="col-sm-9">
+                                        <input type="email" class="form-control" id="email" name="email" placeholder="Email Address" value="{{email}}" 
+                                        ng-model="employee.email" ng-required="true">
+                                        <span class="help-inline" 
+                                        ng-show="frmEmployees.email.$invalid && frmEmployees.email.$touched">Valid Email field is required</span>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="inputEmail3" class="col-sm-3 control-label">Contact Number</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" id="contact_number" name="contact_number" placeholder="Contact Number" value="{{contact_number}}" 
+                                        ng-model="employee.contact_number" ng-required="true">
+                                    <span class="help-inline" 
+                                        ng-show="frmEmployees.contact_number.$invalid && frmEmployees.contact_number.$touched">Contact number field is required</span>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="inputEmail3" class="col-sm-3 control-label">Position</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" id="position" name="position" placeholder="Position" value="{{position}}" 
+                                        ng-model="employee.position" ng-required="true">
+                                    <span class="help-inline" 
+                                        ng-show="frmEmployees.position.$invalid && frmEmployees.position.$touched">Position field is required</span>
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary" id="btn-save" ng-click="save(modalstate, id)" ng-disabled="frmEmployees.$invalid">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
          
       
