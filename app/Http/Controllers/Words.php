7e924e7abbd97ddd9d420174e351c22c4bd7cdf0
paddy <paddy@MAC21.local>
2015-12-21 15:21:17 -0500

    go

--- app/Http/Controllers/Words.php
+++ app/Http/Controllers/Words.php
@@ -17,8 +17,7 @@ class Words extends Controller
     public function index()
     {
      
-                $phrases = DB::table('Dictionary')->select('ID','Source','French','German','Japanese')->get();
-
+                $phrases = DB::table('Dictionary')->paginate(15);
         echo json_encode($phrases,JSON_NUMERIC_CHECK);
 
 
