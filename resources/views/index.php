1701ea0e1cd7a962656e062b94f86850fcddb5a4
paddy <paddy@MAC21.local>
2015-12-22 03:56:57 -0500

    lost

--- resources/views/index.php
+++ resources/views/index.php
@@ -62,11 +62,11 @@
             
 
 <table data-toggle="table" data-sort-name="ID"
-       data-sort-order="desc"
+       data-sort-order="desc" data-show-columns="true"
        data-url="http://192.168.99.100:32786/api/v1/words/show/50">
     <thead>
     <tr>
-        <th data-field="ID" data-sortable="true">ID</th>
+        <th data-field="ID" data-sortable="true" data-switchable="false">ID</th>
         <th data-field="Source">English Word</th>
         <th data-field="French">French</th>
         <th data-field="German">German</th>
