f7e8cbfb8d8c42cb41eed34ef25f44ce72f14b86
paddy <paddy@MAC21.local>
2015-12-22 10:57:43 -0500

    losting

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/benzaiten', function () {
+    return view('index');
+});
+
 
 
 //Route::get('/api/v1/words/', 'Words@index');
