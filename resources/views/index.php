b771a1309752546b12d690c401ad20a1ede87619
paddy <paddy@MAC21.local>
2015-12-22 15:00:21 -0500

    kink

--- resources/views/index.php
+++ resources/views/index.php
@@ -41,7 +41,7 @@
         
     <body>
 
-    <header class="animated slideInDown" style="position:fixed; height:100px; border-bottom:solid 1px black; background-color:#FFF;">
+    <header class="animated slideInDown" style="z-index: 400;position:fixed; height:100px; border-bottom:solid 1px black; background-color:#FFF;">
     
     
     <img src="http://marketing.rakuten.com/img/common/logo/2.gif" width="150"   align="absmiddle" style="float:left;">
