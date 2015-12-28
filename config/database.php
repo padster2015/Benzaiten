18458f2f2158b83e73820a6a17482244d991f522
Patrick Henry <paddy@MAC21.local>
2015-12-28 02:06:28 -0500

    server
    server

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', '172.17.0.3'),
+            'host'      => env('DB_HOST', '172.17.0.2'),
             'database'  => env('DB_DATABASE', 'Benzaiten'),
             'username'  => env('DB_USERNAME', 'maindb'),
             'password'  => env('DB_PASSWORD', 'H097729422a!'),
