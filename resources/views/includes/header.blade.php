ef8a556cbb8824a23dede0eced0b3b138e416cfa
Patrick Henry <paddy@MAC21.local>
2015-12-28 23:51:18 -0500

    new box

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,9 @@
-
-<div  id='response' class="animated fadeIn alert alert-success" role="alert" style="display: none; z-index:5000;">Success</div>
-
-
+<div class="alert alert-warning alert-dismissible fade in" role="alert" style="display: none; z-index:5000;">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+  <strong>Success</strong> the oompa loompas have saved your word correctly
+</div>
 
     <header class="animated slideInDown" style="z-index: 400;position:fixed; height:100px; border-bottom:solid 1px black; background-color:#FFF;">
     
