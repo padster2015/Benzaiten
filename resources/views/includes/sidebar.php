0d62873b31cc0374eebcd692bae48570935fc2b0
Patrick Henry <paddy@MAC21.local>
2015-12-28 23:24:39 -0500

    sidebar update

--- resources/views/includes/sidebar.php
+++ resources/views/includes/sidebar.php
@@ -2,8 +2,14 @@
         <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
             <ul class="nav">
               <li class="active"><a href="#">Home</a></li>
-              <li><a href="#">Link 1</a></li>
-              <li><a href="#">Link 2</a></li>
-              <li><a href="#">Link 3</a></li>              
+              <li><a href="#">French</a></li>
+              <li><a href="#">German</a></li>
+              <li><a href="#">Japanese</a></li> 
+              <li><a href="#">Brazilian</a></li>              
             </ul>
+            <select>
+	<option value="Aff">Affiliate</option>
+	<option value="Dis">Display</option>
+	<option value="Att">Attribution</option>
+	</select>
         </div>
