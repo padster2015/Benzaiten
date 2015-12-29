8f99e73f66f54c8c279c96a08edd5ba5518ef786
Patrick Henry <paddy@MAC21.local>
2015-12-28 23:52:04 -0500

    response

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,4 +1,5 @@
-<div class="alert alert-warning alert-dismissible fade in" role="alert" style="display: none; z-index:5000;">
+
+<div class="alert alert-warning alert-dismissible fade in" id='response' role="alert" style="display: none; z-index:5000;">
   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">&times;</span>
   </button>
