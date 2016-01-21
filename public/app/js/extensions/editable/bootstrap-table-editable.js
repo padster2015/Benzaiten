77eb6798f46f0d235dd245e9206b0fb76a0aee09
Patrick Henry <git@patrickhenry.co.uk>
2016-01-20 19:27:07 -0500

    console gone

--- public/app/js/extensions/editable/bootstrap-table-editable.js
+++ public/app/js/extensions/editable/bootstrap-table-editable.js
@@ -50,7 +50,7 @@
             var _formatter = column.formatter;
             column.formatter = function (value, row, index) {
                 var result = _formatter ? _formatter(value, row, index) : value;
-console.log(row,index);2
+
                 return ['<a href="javascript:void(0)"',
                     ' data-name="' + column.field + '"',
                     ' data-pk="' + row['ID'] + '"',
